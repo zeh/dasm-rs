27042ec53ec534361fddca8822ab6dfaac704262
Zeh Fernando <zeh@zehfernando.com>
2020-08-10 14:24:50 -0400

    Added a new test case for incdir use

---
+++ test/include_dir.asm
@@ -0,0 +1,29 @@
+; Test symbolic includes
+;
+; Ensure that quoted, unquoted, and symbol based includes work.
+;
+; --Mike Saarna
+
+	.processor	6502
+	.org		0
+
+TESTINCA = "include-a.inc"
+PASSFORCEA = PASSFORCEB
+
+	lda #PASSFORCEA
+	incdir "."
+	incdir "../"
+	incdir "incdirtest"
+	include "include-a.inc"
+	include include-a.inc
+	include TESTINCA
+	include "11include.inc"
+	include include-z.inc
+
+	; Non-symbol compliant filenames without quotes...
+	include 11include.inc
+
+PASSFORCEB = 1
+
+
+	.end
