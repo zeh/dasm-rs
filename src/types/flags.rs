22c2c441f026174452772329728dbc78780415d9
Zeh Fernando <zeh@zehfernando.com>
2020-08-11 21:01:00 -0400

    Added flag constants, used for symbol and segment formatting

--- src/types/flags.rs
+++ src/types/flags.rs
@@ -21,3 +21,21 @@ pub mod ReasonCodes {
     pub const PhaseError: u64 = 0b0100_0000_0000_0000;
     pub const BranchOutOfRange: u64 = 0b1000_0000_0000_0000;
 }
+
+// In original C code, used for flags as "SYM_*" but without a clear enum
+pub mod SymbolType {
+    pub const Unknown: u8 = 0b0000_0001;            // 0x01: Value Uknown
+    pub const Referenced: u8 = 0b0000_0100;         // 0x04: Referenced
+    pub const StringResult: u8 = 0b0000_1000;       // 0x08: Result is a string
+    pub const Set: u8 = 0b0001_0000;                // 0x10: SET instruction used
+    pub const Macro: u8 = 0b0010_0000;              // 0x20: Symbol is a macro
+    pub const MasterReference: u8 = 0b0100_0000;    // 0x40: Master reference
+}
+
+// In original C code, used for flags as "SF_*" but without a clear enum
+pub mod SegmentTypes {
+    pub const Unknown: u8 = 0b0000_0001;            // 0x01: ORG unknown
+    pub const Referenced: u8 = 0b0000_0100;         // 0x04: ORG referenced
+    pub const BSS: u8 = 0b0001_0000;                // 0x10: uninitialized area (U flag)
+    pub const RelocatableOrigin: u8 = 0b0010_0000;  // 0x20: Relocatable origin active
+}
