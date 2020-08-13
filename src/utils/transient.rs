2c4feccb08bd7a010b2fb0bad8bdf91aa4ed5f6c
Zeh Fernando <zeh@zehfernando.com>
2020-08-13 14:32:40 -0400

    Added a method to convert address pointers with buffer length to strings

--- src/utils/transient.rs
+++ src/utils/transient.rs
@@ -2,7 +2,7 @@
 use std::os::raw::c_char;
 
 /**
- * Converts a *str (typically used for strings in DASM) to a proper Rust String.
+ * Converts a *str until it finds a char 0 to a proper Rust String.
  * This is likely inneficient, but should suffice for now.
  * FIXME: this should likely be removed in the future, once we drop all uses of *str.
  */
@@ -19,6 +19,24 @@ pub fn str_pointer_to_string(str: *const c_char) -> String {
     return sstr_all;
 }
 
+/**
+ * Converts a *str with a length to a proper Rust String.
+ * This is likely inneficient, but should suffice for now.
+ * FIXME: this should likely be removed in the future, once we drop all uses of *str.
+ */
+ pub fn str_pointer_and_len_to_string(str: *const c_char, len: usize) -> String {
+    // FIXME: can we use CString/Cstr for this?
+    let mut sstr = str;
+    let mut sstr_all = String::from("");
+    unsafe {
+        while sstr_all.len() < len {
+            sstr_all.push_str(std::str::from_utf8(&[*sstr as u8]).unwrap());
+            sstr = sstr.offset(1);
+        }
+    }
+    return sstr_all;
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -28,5 +46,11 @@ mod tests {
     fn test_str_pointer_to_string() {
         assert_eq!(str_pointer_to_string((b"\x00").as_ptr() as *const c_char), String::from(""));
         assert_eq!(str_pointer_to_string((b"Word 123\x00").as_ptr() as *const c_char), String::from("Word 123"));
+    }
+
+    #[test]
+    fn test_str_pointer_and_len_to_string() {
+        assert_eq!(str_pointer_and_len_to_string((b"\x0012345").as_ptr() as *const c_char, 4), String::from("123"));
+        assert_eq!(str_pointer_and_len_to_string((b"Word 123\x00").as_ptr() as *const c_char, 5), String::from("Word "));
 	}
 }
