dc9ed0dc5b7217dc9384bb99562b71b64f47b226
Zeh Fernando <zeh@zehfernando.com>
2020-08-13 21:40:09 -0400

    _filesystem withoiut mut

--- src/utils/filesystem.rs
+++ src/utils/filesystem.rs
@@ -5,7 +5,7 @@ pub fn create_new_file(filename: &str) -> std::io::Result<File> {
 	File::create(filename)
 }
 
-pub fn write_buffer_to_file_maybe(maybe_file: &mut Option<File>, buffer: &[u8]) {
+pub fn write_buffer_to_file_maybe(maybe_file: Option<File>, buffer: &[u8]) {
 	match maybe_file {
 		Some(file) => {
             file.write_all(buffer).expect("Error writing to file");
@@ -14,15 +14,15 @@ pub fn write_buffer_to_file_maybe(maybe_file: &mut Option<File>, buffer: &[u8]) 
 	}
 }
 
-pub fn write_to_file_maybe(maybe_file: &mut Option<File>, message: &str) {
+pub fn write_to_file_maybe(maybe_file: Option<File>, message: &str) {
 	write_buffer_to_file_maybe(maybe_file, message.as_bytes());
 }
 
-pub fn writeln_to_file_maybe(maybe_file: &mut Option<File>, message: &str) {
+pub fn writeln_to_file_maybe(maybe_file: Option<File>, message: &str) {
 	write_to_file_maybe(maybe_file, &([message, "\n"]).concat());
 }
 
-pub fn close_file_maybe(maybe_file: &mut Option<File>) {
+pub fn close_file_maybe(maybe_file: Option<File>) {
 	match maybe_file {
 		Some(file) => {
             file.sync_all().expect("Error closing file");
