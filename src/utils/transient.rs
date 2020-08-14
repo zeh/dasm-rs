b670d45416f713aa3aef901d6ce4cfb9f7b9b64b
Zeh Fernando <zeh@zehfernando.com>
2020-08-14 09:12:47 -0400

    Added a new transient function to convert Strings to *str

--- src/utils/transient.rs
+++ src/utils/transient.rs
@@ -1,4 +1,5 @@
 // FIXME: used for conversion only, remove this once transient functions are not needed anymore
+use std::ffi::CString;
 use std::os::raw::c_char;
 
 /**
@@ -37,6 +38,13 @@ pub fn str_pointer_to_string(str: *const c_char) -> String {
     return sstr_all;
 }
 
+/**
+ * Converts a String to a *str pointer.
+ * FIXME: this should likely be removed in the future, once we drop all uses of *str.
+ */
+pub fn string_to_str_pointer(str: String) -> *mut c_char {
+	CString::new(str).expect("Failed to convert argument into CString.").into_raw()
+}
 
 #[cfg(test)]
 mod tests {
@@ -52,5 +60,13 @@ mod tests {
     fn test_str_pointer_and_len_to_string() {
         assert_eq!(str_pointer_and_len_to_string((b"\x0012345").as_ptr() as *const c_char, 4), String::from("\u{0}123"));
         assert_eq!(str_pointer_and_len_to_string((b"Word 123\x00").as_ptr() as *const c_char, 5), String::from("Word "));
+    }
+
+	#[test]
+	fn test_string_to_str_pointer() {
+		assert_eq!(
+			str_pointer_to_string(string_to_str_pointer(String::from("Word 123"))),
+			str_pointer_to_string((b"Word 123\x00").as_ptr() as *const c_char)
+		);
 	}
 }
