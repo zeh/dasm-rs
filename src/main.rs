872f58baf479e98184ca80f75b0b673f43af2913
Zeh Fernando <zeh@zehfernando.com>
2020-08-17 21:46:01 -0400

    Reorganized main() function a bit

--- src/main.rs
+++ src/main.rs
@@ -2238,13 +2238,16 @@ pub unsafe extern "C" fn passbuffer_cleanup() {
 #[main]
 pub fn main() {
     let mut args: Vec<*mut libc::c_char> = Vec::new();
-    for arg in ::std::env::args() {
-        args.push(::std::ffi::CString::new(arg).expect("Failed to convert argument into CString.").into_raw());
-    };
-    args.push(::std::ptr::null_mut());
+    for arg in std::env::args() {
+        args.push(transient::string_to_str_pointer(arg));
+    }
+    args.push(std::ptr::null_mut());
     unsafe {
-        std::process::exit(main_0((args.len() - 1) as libc::c_int,
-                                    args.as_mut_ptr() as
-                                        *mut *mut libc::c_char) as i32)
+        std::process::exit(
+            main_0(
+                (args.len() - 1) as libc::c_int,
+                args.as_mut_ptr() as *mut *mut libc::c_char
+            ) as i32
+        );
     }
 }
