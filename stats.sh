0367e8533eff0ef00eb86fcef1e9542fbed5fff2
Zeh Fernando <zeh@zehfernando.com>
2020-08-20 08:35:31 -0400

    Build stats also show number of externs used

--- stats.sh
+++ stats.sh
@@ -4,9 +4,10 @@ echo \* ftohex - $(LC_ALL=en_US.UTF-8 numfmt --grouping $(du --apparent-size --b
 echo \* dasm - $(LC_ALL=en_US.UTF-8 numfmt --grouping $(du --apparent-size --block=1 bin/dasm | grep -o -i --regexp='[0-9]*'))b
 echo \* "libc::" - $(grep -o -r 'libc::' src | wc -l)
 echo \* "unsafe" - $(grep -o -r 'unsafe' src | wc -l)
+echo \* "extern" - $(grep -o -r 'extern' src | wc -l)
 echo \* "memcpy" - $(grep -o -r 'memcpy' src | wc -l)
 echo \* "malloc" - $(grep -o -r '\bmalloc' src | wc -l)
-echo \* "printf" - $(grep -o -r 'printf' src | wc -l)
+echo \* "*printf" - $(grep -o -r 'printf' src | wc -l)
 echo \* "no_mangle" - $(grep -o -r 'no_mangle' src | wc -l)
 echo \* "transient::" - $(grep -o -r 'transient::' src | wc -l)
 echo \* "FIXME:" - $(grep -o -r 'FIXME:' src | wc -l)
