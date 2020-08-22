91f505b5f9ad11e6f68e4d3fd429315dc5c84fea
Zeh Fernando <zeh@zehfernando.com>
2020-08-21 22:32:48 -0400

    Added a format_line_tabs() formatting function
    This should replace the somewhat cumbersome tabit() function.

--- src/utils/formatting.rs
+++ src/utils/formatting.rs
@@ -53,6 +53,50 @@ pub fn segment_address_to_string(value: u64, flags: u8) -> String {
 	result
 }
 
+/**
+ * Formats a line, reorganizing tabs into specific column locations.
+ * In original C code, "tabit()" in main.rs
+ */
+pub fn format_line_tabs(line: &str) -> String {
+	let mut result: String = String::new();
+	let mut spaces: i16 = 0;
+	for c in line.chars() {
+		if c == '\n' {
+			break;
+		}
+
+		if c == '\t' {
+			// Optimize out spaces before the tab
+			while spaces > 0 && result.ends_with(" ") {
+				result.remove(result.len() - 1);
+				spaces -= 1
+			}
+			spaces = 0;
+			// Recopy the tab
+			result.push_str("\t");
+		} else {
+			result.push(c);
+		}
+
+		if spaces == 7 && result.ends_with("  ") {
+			let mut spaces_to_remove = spaces;
+			while spaces_to_remove >= 0 && result.ends_with(" ") {
+				spaces_to_remove -= 1;
+				result.remove(result.len() - 1);
+			}
+			result.push_str("\t");
+		}
+		spaces = spaces + 1 & 7
+	}
+
+	while result.len() > 0 && (result.ends_with(" ") || result.ends_with("\t")) {
+		result.remove(result.len() - 1);
+	}
+
+	result.push_str("\n");
+	result
+}
+
 #[cfg(test)]
 mod tests {
 	use super::*;
@@ -80,6 +124,40 @@ mod tests {
 					| SymbolTypes::MasterReference
 			),
 			"b33f ???? str eqm (RS)"
+		);
+	}
+
+	#[test]
+	fn test_format_line_tabs() {
+		assert_eq!(format_line_tabs("a\n"), "a\n");
+		assert_eq!(format_line_tabs("a"), "a\n");
+		assert_eq!(format_line_tabs("ab"), "ab\n");
+		assert_eq!(format_line_tabs("a b"), "a b\n");
+		assert_eq!(format_line_tabs("a  b"), "a  b\n");
+		assert_eq!(format_line_tabs("a   b"), "a   b\n");
+		assert_eq!(format_line_tabs("a    b"), "a    b\n");
+		assert_eq!(format_line_tabs("a     b"), "a     b\n");
+		assert_eq!(format_line_tabs("a      b"), "a      b\n");
+		assert_eq!(format_line_tabs("a       b"), "a\tb\n");
+		assert_eq!(format_line_tabs("a        b"), "a\t b\n");
+		assert_eq!(format_line_tabs("a         b"), "a\t  b\n");
+		assert_eq!(format_line_tabs("a          b"), "a\t   b\n");
+		assert_eq!(format_line_tabs("a           b"), "a\t    b\n");
+		assert_eq!(format_line_tabs("a            b"), "a\t     b\n");
+		assert_eq!(format_line_tabs("a             b"), "a\t      b\n");
+		assert_eq!(format_line_tabs("a              b"), "a\t       b\n");
+		assert_eq!(format_line_tabs("a               b"), "a\t\tb\n");
+		assert_eq!(format_line_tabs("a                b"), "a\t\t b\n");
+		assert_eq!(format_line_tabs("aa               b"), "aa\t\t b\n");
+		assert_eq!(format_line_tabs("aa                b"), "aa\t\t  b\n");
+		assert_eq!(format_line_tabs("aa                 b"), "aa\t\t   b\n");
+		assert_eq!(
+			format_line_tabs("aa               b                  c       d e             f        "),
+			"aa\t\t b\t\t    c\t    d e \t    f\n"
+		);
+		assert_eq!(
+			format_line_tabs("aa               b                  c       d e             f        \n"),
+			"aa\t\t b\t\t    c\t    d e \t    f\n"
 		);
 	}
 }
