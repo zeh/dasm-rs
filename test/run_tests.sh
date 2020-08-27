ae8c0c9c6269982ed1469e50117c9a70b1b9979d
Zeh Fernando <zeh@zehfernando.com>
2020-08-27 09:47:37 -0400

    Reorganized tests and test script
    We now use a single loop for all tests inside the test script (easier to maintain), and test files are organized into folders.

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -12,324 +12,164 @@ SUFFIX=$2
 DASM="${ORIGIN}/dasm"
 FTOHEX="${ORIGIN}/ftohex"
 COLOR_RED="\033[1;31m"
+COLOR_YELLOW="\033[1;93m"
+COLOR_GREEN="\033[1;32m"
 COLOR_RESET="\033[0m"
 FAIL_LABEL="${COLOR_RED}FAIL${COLOR_RESET}"
+SKIP_LABEL="${COLOR_YELLOW}skip${COLOR_RESET}"
+PASS_LABEL="pass"
 
-# 1. General cleanup
-
-echo
-echo "Cleaning up"
-echo
-
-if [[ -z "$SUFFIX" ]]; then
-  echo "... Removing all previously generated files"
-  rm -r ./*.txt
-  rm -r ./*.bin
-  rm -r ./*.hex
-  rm -r ./atari2600/*.txt
-  rm -r ./atari2600/*.bin
-  rm -r ./atari2600/*.hex
-  rm -r ./atari7800/*.txt
-  rm -r ./atari7800/*.bin
-  rm -r ./atari7800/*.hex
-  rm -r ./channel-f/*.txt
-  rm -r ./channel-f/*.bin
-  rm -r ./channel-f/*.hex
-  rm -r ./jeff-jetton-examples/*.txt
-  rm -r ./jeff-jetton-examples/*.bin
-  rm -r ./jeff-jetton-examples/*.hex
-else
-  echo "... Removing all previously generated files with suffix $SUFFIX"
-  rm *$SUFFIX
-fi
-echo
-
-# 2. Standard test cases
-
-echo "Running standard build test cases (strict off)"
-echo
-
-errors=0
-
-for i in *.asm; do
-  i="${i//.bin.ref/.asm}"
-  NAME=$(basename "$i" .asm)
-
-  # Compile .asm into .bin, .list.txt, and symbols.txt
-  $DASM "$i" -f1 -o"$NAME.bin$SUFFIX" -l"$NAME.list.txt$SUFFIX" -s"$NAME.symbols.txt$SUFFIX" -DINEEPROM 2>&1 | \
-    tee "$NAME.stdout.txt$SUFFIX" >/dev/null
-    # | \grep -vE 'error|Complete|Fatal|Warning^?'
-  $DASM "$i" -f1 -v4 -o/dev/null -DINEEPROM > "$NAME.stdout-verbose.txt$SUFFIX" 2>&1
-  $DASM "$i" -f1 -d1 -o/dev/null -DINEEPROM > "$NAME.stdout-debug.txt$SUFFIX" 2>&1
-
-  # Obfuscate memory addresses (they change every time)
-  sed -i -r 's/^[0-9a-f]{12} /0MEMORYADDR0 /g' "$NAME.stdout-debug.txt$SUFFIX"
-
-  # Obfuscate line numbers for error messages (useless)
-  sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout.txt$SUFFIX"
-  sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout-verbose.txt$SUFFIX"
-  sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout-debug.txt$SUFFIX"
-
-  # Generate .hex file from .bin
-  $FTOHEX 1 "$NAME.bin$SUFFIX" "$NAME.hex$SUFFIX"
-
-  # Display results
-  echo -ne "  * ${NAME}: "
-
-  if [[ -z "$SUFFIX" ]]; then
-    cmp -s "$NAME.bin" "$NAME.bin.ref"
-    if [ $? == 0 ]; then
-      echo -ne "bin pass; "
-    else
-      echo -ne "bin $FAIL_LABEL; "
-      errors=$((errors + 1))
-    fi
-    cmp -s "$NAME.hex" "$NAME.hex.ref"
-    if [ $? == 0 ]; then
-      echo -ne "hex pass; "
-    else
-      echo -ne "hex $FAIL_LABEL; "
-      errors=$((errors + 1))
-    fi
-    cmp -s "$NAME.list.txt" "$NAME.list.txt.ref"
-    if [ $? == 0 ]; then
-      echo -ne "list pass; "
-    else
-      echo -ne "list $FAIL_LABEL; "
-      errors=$((errors + 1))
-    fi
-    cmp -s "$NAME.symbols.txt" "$NAME.symbols.txt.ref"
-    if [ $? == 0 ]; then
-      echo -ne "symbols pass; "
-    else
-      echo -ne "symbols $FAIL_LABEL; "
-      errors=$((errors + 1))
-    fi
-    cmp -s "$NAME.stdout.txt" "$NAME.stdout.txt.ref"
-    if [ $? == 0 ]; then
-      echo -ne "stdout pass; "
-    else
-      echo -ne "stdout $FAIL_LABEL; "
-      errors=$((errors + 1))
-    fi
-    cmp -s "$NAME.stdout-verbose.txt" "$NAME.stdout-verbose.txt.ref"
-    if [ $? == 0 ]; then
-      echo -ne "stdout-verbose pass; "
-    else
-      echo -ne "stdout-verbose $FAIL_LABEL; "
-      errors=$((errors + 1))
-    fi
-    cmp -s "$NAME.stdout-debug.txt" "$NAME.stdout-debug.txt.ref"
-    if [ $? == 0 ]; then
-      echo -ne "stdout-debug pass. "
-    else
-      echo -ne "stdout-debug $FAIL_LABEL. "
-      errors=$((errors + 1))
-    fi
-  else
-    echo -ne "generated with suffix $SUFFIX"
-  fi
-  echo
-done
+# 1. Generate files
 
 echo
-if (( errors > 0 )); then
-  echo "...$errors errors detected."
-else
-  echo "...no errors detected."
-fi
-echo
-
-# 3. Test cases where an error is expected
-
-echo "Running error build test cases (strict on)"
-echo
-
-errors=0
-
-for i in *.fail; do
-  i="${i//.fail/.asm}"
-  NAME=$(basename "$i" .asm)
-
-  # Compile .asm into .bin, .list.txt, and symbols.txt
-  $DASM "$i" -S -f1 -o"$NAME.strict.bin$SUFFIX" -l"$NAME.list.strict.txt$SUFFIX" -s"$NAME.symbols.strict.txt$SUFFIX" -DINEEPROM 2>&1 | \
-    tee "$NAME.stdout.strict.txt$SUFFIX" >/dev/null
-    # | \grep -vE 'error|Complete|Fatal|Warning^?'
-  $DASM "$i" -S -f1 -v4 -o/dev/null -DINEEPROM > "$NAME.stdout-verbose.strict.txt$SUFFIX" 2>&1
-  $DASM "$i" -S -f1 -d1 -o/dev/null -DINEEPROM > "$NAME.stdout-debug.strict.txt$SUFFIX" 2>&1
-
-  # Obfuscate memory addresses (they change every time)
-  sed -i -r 's/^[0-9a-f]{12} /0MEMORYADDR0 /g' "$NAME.stdout-debug.strict.txt$SUFFIX"
-
-  # Obfuscate line numbers for error messages (useless)
-  sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout.strict.txt$SUFFIX"
-  sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout-verbose.strict.txt$SUFFIX"
-  sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout-debug.strict.txt$SUFFIX"
-
-  # Display results
-  echo -ne "  * ${NAME}: "
-
-  if [[ -z "$SUFFIX" ]]; then
-    grep error "$NAME.list.strict.txt" 2>&1 >/dev/null
-    if [ $? == 0 ]; then
-      echo -ne "error trigger pass; "
-    else
-      echo -ne "error trigger $FAIL_LABEL; "
-      errors=$((errors + 1))
-    fi
-    cmp -s "$NAME.list.strict.txt" "$NAME.list.strict.txt.ref"
-    if [ $? == 0 ]; then
-      echo -ne "list pass; "
-    else
-      echo -ne "list $FAIL_LABEL; "
-      errors=$((errors + 1))
-    fi
-    if [ -f "$NAME.symbols.strict.txt.ref" ]; then
-      cmp -s "$NAME.symbols.strict.txt" "$NAME.symbols.strict.txt.ref"
-      if [ $? == 0 ]; then
-        echo -ne "symbols pass; "
-      else
-        echo -ne "symbols $FAIL_LABEL; "
-        errors=$((errors + 1))
-      fi
-    else
-      if [ -f "$NAME.symbols.strict.txt" ]; then
-        echo -ne "symbols $FAIL_LABEL (new); "
-        errors=$((errors + 1))
-      else
-        echo -ne "symbols skip; "
-      fi
-    fi
-    cmp -s "$NAME.stdout.strict.txt" "$NAME.stdout.strict.txt.ref"
-    if [ $? == 0 ]; then
-      echo -ne "stdout pass; "
-    else
-      echo -ne "stdout $FAIL_LABEL; "
-      errors=$((errors + 1))
-    fi
-    cmp -s "$NAME.stdout-verbose.strict.txt" "$NAME.stdout-verbose.strict.txt.ref"
-    if [ $? == 0 ]; then
-      echo -ne "stdout-verbose pass; "
-    else
-      echo -ne "stdout-verbose $FAIL_LABEL; "
-      errors=$((errors + 1))
-    fi
-    cmp -s "$NAME.stdout-debug.strict.txt" "$NAME.stdout-debug.strict.txt.ref"
-    if [ $? == 0 ]; then
-      echo -ne "stdout-debug pass. "
-    else
-      echo -ne "stdout-debug $FAIL_LABEL. "
-      errors=$((errors + 1))
-    fi
-  else
-    echo -ne " generated with suffix $SUFFIX"
-  fi
-  echo
-done
-
-echo
-if (( errors > 0 )); then
-  echo "...$errors errors detected."
-else
-  echo "...no errors detected."
-fi
-echo
-
-# 4. Special cases
-
-echo "Running special build test cases"
+echo "Running build test cases"
 echo
 
 custom_files=(
-  "atari2600/boing26.asm"
-  "atari7800/spritesample.asm"
-  "channel-f/lights.asm"
-  "channel-f/tetris.asm"
+  "basic/*.asm"
+  "basic-strict/*.asm"
+  "atari2600/*.asm"
+  "atari7800/*.asm"
+  "channel-f/*.asm"
   "jeff-jetton-examples/*.asm"
 )
 custom_params=(
+  "-f1 -Ibasic -Ibasic/incdirtest"
+  "-f1 -Ibasic -Ibasic/incdirtest -S"
   "-f3 -I../machines/atari2600/"
   "-f3 -I../machines/atari7800/"
   "-f3 -I../../machines/channel-f/"
-  "-f3 -I../../machines/channel-f/"
   "-f3"
+)
+wants_error=(
+  "0"
+  "1"
+  "0"
+  "0"
+  "0"
+  "0"
 )
 
 errors=0
 
 for ((i = 0; i < ${#custom_files[@]}; i++)); do
-  FILE_MASK="${custom_files[i]}"
+  FILE_MASK=${custom_files[i]}
+  DIR_NAME=${FILE_MASK%/*}
+  STRICT_SUFFIX=""
+
+  if [[ "${wants_error[i]}" == "1" ]]; then
+    STRICT_SUFFIX=".strict"
+  fi
+
+  # Removes files first
+  if [[ -z "$SUFFIX" ]]; then
+    rm -r ${DIR_NAME}/*.txt
+    rm -r ${DIR_NAME}/*.bin
+    rm -r ${DIR_NAME}/*.hex
+  else
+    rm -r ${DIR_NAME}/*$SUFFIX
+  fi
 
   for j in ${FILE_MASK}; do
     NAME="${j//.asm/}"
 
     # Compile .asm into .bin, .list.txt, and symbols.txt
-    $DASM "$NAME.asm" ${custom_params[i]} -o"$NAME.bin$SUFFIX" -l"$NAME.list.txt$SUFFIX" -s"$NAME.symbols.txt$SUFFIX" -DINEEPROM 2>&1 | \
-      tee "$NAME.stdout.txt$SUFFIX" >/dev/null
+    $DASM "$NAME.asm" ${custom_params[i]} -o"$NAME$STRICT_SUFFIX.bin$SUFFIX" -l"$NAME.list$STRICT_SUFFIX.txt$SUFFIX" -s"$NAME.symbols$STRICT_SUFFIX.txt$SUFFIX" -DINEEPROM 2>&1 | \
+      tee "$NAME.stdout$STRICT_SUFFIX.txt$SUFFIX" >/dev/null
       # | \grep -vE 'error|Complete|Fatal|Warning^?'
-    $DASM "$NAME.asm" -v4 ${custom_params[i]} -o/dev/null -DINEEPROM > "$NAME.stdout-verbose.txt$SUFFIX" 2>&1
-    $DASM "$NAME.asm" -d1 ${custom_params[i]} -o/dev/null -DINEEPROM > "$NAME.stdout-debug.txt$SUFFIX" 2>&1
+    $DASM "$NAME.asm" -v4 ${custom_params[i]} -o/dev/null -DINEEPROM > "$NAME.stdout-verbose$STRICT_SUFFIX.txt$SUFFIX" 2>&1
+    $DASM "$NAME.asm" -d1 ${custom_params[i]} -o/dev/null -DINEEPROM > "$NAME.stdout-debug$STRICT_SUFFIX.txt$SUFFIX" 2>&1
 
     # Obfuscate memory addresses (they change every time)
-    sed -i -r 's/^[0-9a-f]{12} /0MEMORYADDR0 /g' "$NAME.stdout-debug.txt$SUFFIX"
+    sed -i -r 's/^[0-9a-f]{12} /0MEMORYADDR0 /g' "$NAME.stdout-debug$STRICT_SUFFIX.txt$SUFFIX"
 
     # Obfuscate line numbers for error messages (useless)
-    sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout.txt$SUFFIX"
-    sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout-verbose.txt$SUFFIX"
-    sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout-debug.txt$SUFFIX"
+    sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout$STRICT_SUFFIX.txt$SUFFIX"
+    sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout-verbose$STRICT_SUFFIX.txt$SUFFIX"
+    sed -i -r 's/.rs:[0-9]+:[0-9]+$/.rs:0000:0/g' "$NAME.stdout-debug$STRICT_SUFFIX.txt$SUFFIX"
 
     # Generate .hex file from .bin
-    $FTOHEX 1 "$NAME.bin$SUFFIX" "$NAME.hex$SUFFIX"
+    $FTOHEX 1 "$NAME$STRICT_SUFFIX.bin$SUFFIX" "$NAME$STRICT_SUFFIX.hex$SUFFIX"
+
+    # Fixes typos in original
+    if [ -n "$SUFFIX" ]; then
+      sed -i -r 's/defintion/definition/g' ${DIR_NAME}/*$SUFFIX
+    fi
 
     # Display results
     echo -ne "  * ${NAME}: "
 
     if [[ -z "$SUFFIX" ]]; then
-      cmp -s "$NAME.bin" "$NAME.bin.ref"
-      if [ $? == 0 ]; then
-        echo -ne "bin pass; "
+      # Compare binaries, or error if expected
+      if [[ "${wants_error[i]}" == "0" ]]; then
+        cmp -s "$NAME.bin" "$NAME.bin.ref"
+        if [ $? == 0 ]; then
+          echo -ne "bin $PASS_LABEL; "
+        else
+          echo -ne "bin $FAIL_LABEL; "
+          errors=$((errors + 1))
+        fi
+        cmp -s "$NAME.hex" "$NAME.hex.ref"
+        if [ $? == 0 ]; then
+          echo -ne "hex $PASS_LABEL; "
+        else
+          echo -ne "hex $FAIL_LABEL; "
+          errors=$((errors + 1))
+        fi
       else
-        echo -ne "bin $FAIL_LABEL; "
-        errors=$((errors + 1))
+        grep error "$NAME.list$STRICT_SUFFIX.txt" 2>&1 >/dev/null
+        if [ $? == 0 ]; then
+          echo -ne "error trigger $PASS_LABEL; "
+        else
+          echo -ne "error trigger $FAIL_LABEL; "
+          errors=$((errors + 1))
+        fi
       fi
-      cmp -s "$NAME.hex" "$NAME.hex.ref"
+
+      # Compare list
+      cmp -s "$NAME.list$STRICT_SUFFIX.txt" "$NAME.list$STRICT_SUFFIX.txt.ref"
       if [ $? == 0 ]; then
-        echo -ne "hex pass; "
-      else
-        echo -ne "hex $FAIL_LABEL; "
-        errors=$((errors + 1))
-      fi
-      cmp -s "$NAME.list.txt" "$NAME.list.txt.ref"
-      if [ $? == 0 ]; then
-        echo -ne "list pass; "
+        echo -ne "list $PASS_LABEL; "
       else
         echo -ne "list $FAIL_LABEL; "
         errors=$((errors + 1))
       fi
-      cmp -s "$NAME.symbols.txt" "$NAME.symbols.txt.ref"
-      if [ $? == 0 ]; then
-        echo -ne "symbols pass; "
+
+      # Compare symbols
+      if [ -f "$NAME.symbols$STRICT_SUFFIX.txt.ref" ]; then
+        cmp -s "$NAME.symbols$STRICT_SUFFIX.txt" "$NAME.symbols$STRICT_SUFFIX.txt.ref"
+        if [ $? == 0 ]; then
+          echo -ne "symbols $PASS_LABEL; "
+        else
+          echo -ne "symbols $FAIL_LABEL; "
+          errors=$((errors + 1))
+        fi
       else
-        echo -ne "symbols $FAIL_LABEL; "
-        errors=$((errors + 1))
+        echo -ne "symbols $SKIP_LABEL; "
       fi
-      cmp -s "$NAME.stdout.txt" "$NAME.stdout.txt.ref"
+
+      # Compare stdout
+      cmp -s "$NAME.stdout$STRICT_SUFFIX.txt" "$NAME.stdout$STRICT_SUFFIX.txt.ref"
       if [ $? == 0 ]; then
-        echo -ne "stdout pass; "
+        echo -ne "stdout $PASS_LABEL; "
       else
         echo -ne "stdout $FAIL_LABEL; "
         errors=$((errors + 1))
       fi
-      cmp -s "$NAME.stdout-verbose.txt" "$NAME.stdout-verbose.txt.ref"
+
+      # Compare stdout (verbose)
+      cmp -s "$NAME.stdout-verbose$STRICT_SUFFIX.txt" "$NAME.stdout-verbose$STRICT_SUFFIX.txt.ref"
       if [ $? == 0 ]; then
-        echo -ne "stdout-verbose pass; "
+        echo -ne "stdout-verbose $PASS_LABEL; "
       else
         echo -ne "stdout-verbose $FAIL_LABEL; "
         errors=$((errors + 1))
       fi
-      cmp -s "$NAME.stdout-debug.txt" "$NAME.stdout-debug.txt.ref"
+
+      # Compare stdout (debug)
+      cmp -s "$NAME.stdout-debug$STRICT_SUFFIX.txt" "$NAME.stdout-debug$STRICT_SUFFIX.txt.ref"
       if [ $? == 0 ]; then
-        echo -ne "stdout-debug pass. "
+        echo -ne "stdout-debug $PASS_LABEL. "
       else
         echo -ne "stdout-debug $FAIL_LABEL. "
         errors=$((errors + 1))
@@ -339,9 +179,9 @@ for ((i = 0; i < ${#custom_files[@]}; i++)); do
     fi
     echo
   done
+  echo
 done
 
-echo
 if (( errors > 0 )); then
   echo "...$errors errors detected."
 else
@@ -349,12 +189,20 @@ else
 fi
 echo
 
-# 5. Other
+# 2. Other cases
 
+echo
 echo "Other cases"
 echo
 
 errors=0
+
+# Removes files first
+if [[ -z "$SUFFIX" ]]; then
+  rm *.txt
+else
+  rm *$SUFFIX
+fi
 
 NAME="dasm-no-args"
 
@@ -368,7 +216,7 @@ echo -ne "  * ${NAME}: "
 if [[ -z "$SUFFIX" ]]; then
   cmp -s "$NAME.stdout.txt" "$NAME.stdout.txt.ref"
   if [ $? == 0 ]; then
-    echo -ne "stdout pass. "
+    echo -ne "stdout $PASS_LABEL. "
   else
     echo -ne "stdout $FAIL_LABEL. "
     errors=$((errors + 1))
@@ -385,17 +233,3 @@ else
   echo "...no errors detected."
 fi
 echo
-echo
-
-# 6. Fixes file errors for comparison
-
-echo "Fixing snapshots"
-echo
-
-if [ -n "$SUFFIX" ]; then
-  # Typos in original
-  sed -i -r 's/defintion/definition/g' *$SUFFIX
-  sed -i -r 's/defintion/definition/g' ./atari2600/*$SUFFIX
-  sed -i -r 's/defintion/definition/g' ./atari7800/*$SUFFIX
-  sed -i -r 's/defintion/definition/g' ./channel-f/*$SUFFIX
-fi
