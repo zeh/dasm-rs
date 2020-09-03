171265336e4953bb6c25de62623683e036b9a992
Zeh Fernando <zeh@zehfernando.com>
2020-09-03 14:42:10 -0400

    ___TEST ASM

--- test/basic/doublemacro.asm
+++ test/basic/doublemacro.asm
@@ -1,12 +1,14 @@
-; Test case for double macros 
+; Test case for double macros
 
 	processor 6502
 
  MAC testmac
 	lda #1
+	ds	33, $a5
  ENDM
  MAC testmac
 	lda #1
+	ds	33, $a5
  ENDM
 
 	ORG $f800
